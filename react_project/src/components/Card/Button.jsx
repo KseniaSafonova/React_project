42935c296eebf8031e4531f74dd65700fd94a09d
KseniaSafonova <kseniatsn@gmail.com>
2021-09-01 14:53:44 +0300

    wednesday

--- react_project/src/components/Card/Button.jsx
+++ react_project/src/components/Card/Button.jsx
@@ -4,18 +4,13 @@ import styles from './Button.module.css'
 function Button(props) {
   const [pressed, setPressed] = useState(false);
 
-  const handChange = () => {
-    setPressed(!pressed);
-  }
-
   return (
     <div>
       {
         pressed
-          ? <div className={styles.element}>{props.russian}</div>
-          : (<button className={styles.button} onClick={handChange}>Посмотреть перевод</button>)
+          ? <div className={styles.element} onBlur={() => { setPressed(false) }} >{props.russian}</div>
+          : <button className={styles.button} onClick={() => { setPressed(true) }}>Показать перевод</button>
       }
-
     </div>
   )
 }
