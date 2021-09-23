69da4816ac61a1f7dd025ec97effc3316e195fc8
KseniaSafonova <kseniatsn@gmail.com>
2021-09-23 13:36:26 +0300

    thursday

--- react_project/src/components/Card/Button.jsx
+++ react_project/src/components/Card/Button.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import styles from './Button.module.css'
 
-function Button(props) {
-  const [pressed, setPressed] = useState(false);
 
+function Button(props, numberChange) {
+  const [pressed, setPressed] = useState(false);
   return (
     <div>
       {
         pressed
-          ? <div tabIndex={0} className={styles.element} onClick={() => { setPressed(false) }} >{props.russian}</div>
+          ? <div tabIndex={0} className={styles.element} onClick={() => { setPressed(false) }, numberChange()} >{props.russian}</div>
           : <button className={styles.button} onClick={() => { setPressed(true) }}>Показать перевод</button>
       }
     </div>
--- react_project/src/components/Game/CardShell.jsx
+++ react_project/src/components/Game/CardShell.jsx
@@ -2,19 +2,26 @@
 import Card from '../Card/Card';
 import Button from '../Card/Button';
 import styles from './CardShell.module.css'
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const CardShell = ({ showNext, showPrevious, number, data, dataLenght }) => {
+    const { wordCount, countChange } = useState(0);
+
+    const handleChange = () => {
+        countChange(wordCount + 1);
+    }
+
     return (
         <React.Fragment>
             <div className={styles.block}>
+                <div>Изучено {wordCount} слов</div>
                 <button className={styles.button} onClick={showPrevious}>Back</button>
                 <Card
                     styles={styles.element}
                     english={data[number].english}
                     transcription={data[number].transcription}>
-                    <Button russian={data[number].russian} />
+                    <Button russian={data[number].russian} numberChange={handleChange} />
                 </Card>
                 <button className={styles.button} onClick={showNext}>Forward</button>
             </div>
