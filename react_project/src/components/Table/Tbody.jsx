9cfd936738962d6d6fae4154962104441f69a5fb
KseniaSafonova <kseniatsn@gmail.com>
2021-10-10 20:00:11 +0300

    sunday

--- react_project/src/components/Table/Tbody.jsx
+++ react_project/src/components/Table/Tbody.jsx
@@ -5,25 +5,24 @@ import Context from './../../Context'
 
 const Tbody = () => {
 
-    //const value = useContext(Context);
-    //console.log(value)
+    const value = useContext(Context);
+    console.log(value)
 
     return (
-        <Context.Consumer>
-            {value =>
-                < tbody >
-                    {
-                        words.map((word) =>
-                            <TableString
-                                id={word.id}
-                                english={word.english}
-                                transcription={word.transcription}
-                                russian={word.russian}
-                                tags={word.tags} />)
-                    }
-                </tbody >
+
+        < tbody >
+            {
+                words.map((word) =>
+                    <TableString
+                        id={word.id}
+                        english={word.english}
+                        transcription={word.transcription}
+                        russian={word.russian}
+                        tags={word.tags} />)
             }
-        </Context.Consumer>
+        </tbody >
+
+
 
 
     )
