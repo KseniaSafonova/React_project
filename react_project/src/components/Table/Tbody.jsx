3247bf0703c32d51639f96e300db729f1f807971
KseniaSafonova <kseniatsn@gmail.com>
2021-11-26 20:18:07 +0300

    +

--- react_project/src/components/Table/Tbody.jsx
+++ react_project/src/components/Table/Tbody.jsx
@@ -3,23 +3,26 @@ import { observer, inject } from 'mobx-react';
 import { useEffect } from "react";
 import WordsStore from "../../store/WordsStore";
 
-export default function Tbody = inject(['WordsStore'])(observer(({ WordsStore }) => {
+const Tbody = inject(['WordsStore'])(observer(({ WordsStore }) => {
     const { words, isLoaded, fetchWords } = WordsStore;
     useEffect(() => {
         if (!isLoaded) {
             fetchWords()
         }
     })
+    console.log(WordsStore.words);
+    console.log(words)
     // return { words };
-    const listWords = WordsStore.words;
 
     return (
         <tbody>
             {
-                listWords.map((word) =>
+                WordsStore.words.map((word) =>
                     <TableString id={word.id} english={word.english} transcription={word.transcription} russian={word.russian} tags={word.tags} />)
             }
         </tbody>
     );
 }));
 
+
+export default Tbody;
